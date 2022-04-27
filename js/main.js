80c506d75684bee47523c3fdbaf5d3afc9a774ba
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-04-27 13:46:14 +0200

    add title and password

--- js/main.js
+++ js/main.js
@@ -6,4 +6,8 @@ const lastName = prompt("Qual è il tuo cognome?");
 const favoriteColor = prompt("Qual è il tuo colore preferito?");
 // generate password from previous prompts and by appending '21' at the end
 const password = `${name}${lastName}${favoriteColor}21`;
-console.log(password);
+
+// welcome the user
+document.getElementById("welcome").innerHTML = `Ciao ${name}!<br>La tua password generata è:`;
+// show the password
+document.getElementById("password").innerHTML = `${password}`;
