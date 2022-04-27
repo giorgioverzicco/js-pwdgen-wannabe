9f33a7d4a061a0d15c38c72561b5028505b10f9d
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-04-27 13:13:24 +0200

    add generator algorithm

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// prompt user for his name
+const name = prompt("Qual è il tuo nome?");
+// prompt user for his last name
+const lastName = prompt("Qual è il tuo cognome?");
+// prompt user for his favorite color
+const favoriteColor = prompt("Qual è il tuo colore preferito?");
+// generate password from previous prompts and by appending '21' at the end
+const password = `${name}${lastName}${favoriteColor}21`;
+console.log(password);
